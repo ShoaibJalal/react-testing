27f5fa95e6818b639419ca2ebbdd200435210961
Shoaib Jalal <syedshoaibjalal@outlook.com>
2024-07-30 12:40:50 +0300

    testing user interactions and React State

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import { useState } from "react";
 
 const Greeting = () => {
+  const [changeParagraph, setChangeParagraph] = useState(false);
+  const changeParagraphHandler = () => {
+    setChangeParagraph(true);
+  };
+
   return (
     <div>
-      <h2>Hello World</h2>
-      <p>Welcome to Unit Testing</p>
+      <h2>Greetings Testers</h2>
+      {!changeParagraph && <p>Welcome to Unit Testing</p>}
+      {changeParagraph && <p>Your Paragraph Text has Changed.</p>}
+      <button onClick={changeParagraphHandler}>Change Paragraph</button>
     </div>
-  )
-}
+  );
+};
 
-export default Greeting
+export default Greeting;
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,57 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event";
 
-test("renders a Hello World Text", () => {
-  //Arrang
-  render(<Greeting />);
+describe("Greeting Component Feature", () => {
+  test("renders a [Hello World] Text", () => {
+    //Arrang
+    render(<Greeting />);
 
-  //Act
-  //...nothing
+    //Act
+    //...nothing
 
-  //Assert
-  const helloWorldElement = screen.getByText("Hello World");
-  expect(helloWorldElement).toBeInTheDocument();
+    //Assert
+    const helloWorldElement = screen.getByText("Greetings Testers");
+    expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders [Unit Test Paragraph] when Button is not clicked", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    //...pending
+
+    //Assert
+    const paragraphElement = screen.getByText("Welcome to Unit Testing", {
+      exact: false,
+    });
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  test("renders [changed] text if button is clicked", () => {
+    //Arrange
+    render(<Greeting />);
+
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    //Assert
+    const paragraphElement = screen.getByText("Text has Changed", {
+      exact: false,
+    });
+    expect(paragraphElement).toBeInTheDocument();
+  });
+
+  test("should not render [Welcome to Unit Testing] text if button is clicked", () => {
+    //Arrange
+    render(<Greeting />);
+    //Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    //Assert
+    const paragraphElement = screen.queryByText("Welcome to Unit Testing", { exact: false });
+    //expect(paragraphElement).not.toBeInTheDocument();
+    expect(paragraphElement).toBeNull();
+  });
 });
